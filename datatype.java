445adbab1787592a8786fb52d9df70384d5ae562
B153Prem <sirsikarp@gmail.com>
2023-03-17 20:11:54 +0530

    extra method add and call the object

--- datatype.java
+++ datatype.java
@@ -13,11 +13,21 @@ public void m1()
 {
 	System.out.println("Hello");
 }
+public void m2()
+{
+      System.out.println("Primitive Data Types");
+}
+public void m3()
+{
+      System.out.println("Non-primitive Data Types");
+}
 public static void main(String[] args)
 {
 	System.out.println("Hii");
 	A a = new A();
 	a.m1();
+      a.m2();
+      a.m3();
 	System.out.println(a.i);
 	System.out.println(a.s);
 	System.out.println(a.l);
