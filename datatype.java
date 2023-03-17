a1d936c5cff8a9b0abf8e870c9ccad56487f6d88
meghraj2410 <meghrajmane2424@gmail.com>
2023-03-17 15:54:26 +0530

    files Added

---
+++ datatype.java
@@ -0,0 +1,30 @@
+class A
+{
+	int i;
+	short s;
+	long l;
+	float f;
+	double d;
+	char c;
+	boolean bo;
+	byte by;
+
+public void m1()
+{
+	System.out.println("Hello");
+}
+public static void main(String[] args)
+{
+	System.out.println("Hii");
+	A a = new A();
+	a.m1();
+	System.out.println(a.i);
+	System.out.println(a.s);
+	System.out.println(a.l);
+	System.out.println(a.f);
+	System.out.println(a.d);
+	System.out.println(a.c);
+	System.out.println(a.bo);
+	System.out.println(a.by);
+}
+}
